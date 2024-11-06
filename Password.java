565b24c516aca2eaa8d7db1a246db9bd4d8d109d
aisaxytsaeva <aisaxytsaeva@mail.ru>
2024-11-06 18:17:13 +0300

    password.java fixing

--- Password.java
+++ Password.java
@@ -2,15 +2,36 @@ import java.util.regex.*;
 
 public class Password {
     public static void main(String[] args) {
-        String password = "1Ejuob6431";
+        String password = "1Ejuob6431jfeijfdihfdhfihfeh";
         try {
-            Pattern pattern = Pattern.compile("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])[A-Za-z0-9]{8,16}");
-            Matcher matcher = pattern.matcher(password);
-            if(matcher.matches()){
-                System.out.println("Correct password");
-            }else{
-                System.out.println("Incorrect password");
+            Pattern patternUpperCase = Pattern.compile("[A-Z]");
+            Matcher matcherUpperCase = patternUpperCase.matcher(password);
+        
+    
+            Pattern patternLowerCase = Pattern.compile("[a-z]");
+            Matcher matcherLowerCase = patternLowerCase.matcher(password);
+        
+
+            Pattern patternDigit = Pattern.compile("\\d");
+            Matcher matcherDigit = patternDigit.matcher(password);
+        
+
+            Pattern patternLength = Pattern.compile(".{8,16}");
+            Matcher matcherLength = patternLength.matcher(password);
+        
+        
+            if (!matcherUpperCase.find()) {
+                System.out.println("Пароль должен содержать хотя бы одну заглавную букву.");
+            } else if (!matcherLowerCase.find()) {
+                System.out.println("Пароль должен содержать хотя бы одну строчную букву.");
+            } else if (!matcherDigit.find()) {
+                System.out.println("Пароль должен содержать хотя бы одну цифру.");
+            } else if (!matcherLength.matches()) {
+                System.out.println("Длина пароля должна быть от 8 до 16 символов.");
+            } else {
+                System.out.println("Правильный пароль!");
             }
+
         }catch (PatternSyntaxException e) {
             System.out.println("Incorrect pattern syntax " + e.getMessage());
         } catch (Exception e) {
@@ -20,3 +41,4 @@ public class Password {
 
     }
 }
+
